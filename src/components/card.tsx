fc3411ad047c7e00ba672f0b1010785e58388d3d
Ruben <>
2024-01-30 03:37:51 +0200

    add: initial files

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,7 +3,7 @@ import "./css/card.css";
 function Card() {
   return (
     <div className="card">
-      <img src="/images/illustration-article.svg" alt="illustration article" />
+      <img src="./images/illustration-article.svg" alt="illustration article" />
       <br />
       <br />
       <div className="text">
@@ -21,7 +21,7 @@ function Card() {
         </p>
         <br />
         <div className="avatar">
-          <img src="/images/image-avatar.webp" alt="" />
+          <img src="./images/image-avatar.webp" alt="" />
           <p>Grey Hooper</p>
         </div>
       </div>
